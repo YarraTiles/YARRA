8eb8e2a3b90673974055cadcaccadeccf2db53f2
YarraTiles <141019729+YarraTiles@users.noreply.github.com>
2023-08-01 19:19:18 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
 	//Owl Carousel
 	$('.banner-carousel').owlCarousel({
 		items: 1,
@@ -28,3 +29,15 @@ $(window).scroll(function () {
 	}
 });
 
+$(window).on('load', function () {
+	// Get the preloader element using jQuery
+	const $preloader = $('#preloader');
+	// Get all other sections using jQuery
+	const $sections = $('section,footer'); // Add more sections if needed
+
+	// Hide the preloader after the entire website is loaded
+	$preloader.fadeOut(500, function () {
+		// Animation complete, show all other sections
+		$sections.fadeIn(500);
+	});
+});
