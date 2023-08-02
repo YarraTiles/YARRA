c959c4af319074b859290a9b78d2d35aa089df42
YarraTiles <141019729+YarraTiles@users.noreply.github.com>
2023-08-02 20:23:08 +0600

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+	$(".logo").animate({
+		top: "-100px",
+		opacity: 0
+	}, 1000, function () {
+		$(".preloader").slideUp(500);
+	});
+
 	//Owl Carousel
 	$('.banner-carousel').owlCarousel({
 		items: 1,
@@ -9,12 +16,12 @@ $(document).ready(function () {
 		nav: false,
 		dots: true,
 		autoplay: true,
-		autoplayTimeout: 3000,
+		autoplayTimeout: 4000,
 		smartSpeed: 1000,
-
 	});
 });
 
+// HEADER
 $(window).scroll(function () {
 	if ($(this).scrollTop() > 1) {
 		$('.header-area').addClass("sticky");
@@ -27,17 +34,4 @@ $(window).scroll(function () {
 	} else {
 		$('.header-area').removeClass("hide-header");
 	}
-});
-
-$(window).on('load', function () {
-	// Get the preloader element using jQuery
-	const $preloader = $('#preloader');
-	// Get all other sections using jQuery
-	const $sections = $('section,footer'); // Add more sections if needed
-
-	// Hide the preloader after the entire website is loaded
-	$preloader.fadeOut(500, function () {
-		// Animation complete, show all other sections
-		$sections.fadeIn(500);
-	});
 });
